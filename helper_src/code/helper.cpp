00e7b2a439380a7871b01e3baab4dd00401c1a8b
alexamarki <amarkin2006@gmail.com>
2024-05-08 10:07:40 +0300

    Proto version of the helper code is finally complete (after some unexpected troubles with SQLite). A protoype version of the database is complete, will now switch onto coding the switch from this "proto-DB" to the one outlined in db_viz.md

---
+++ helper_src/code/helper.cpp
@@ -0,0 +1,172 @@
+#include "helper.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+
+int main() {
+    // Step 1: trimming data
+    // 1.1: trim ratings
+    // std::ifstream inputFile("../helper_src/data/title.ratings.tsv");
+    // std::ofstream outputFile("../helper_src/data/title.ratings_2.tsv");
+    // std::vector<std::string> acceptableMovies;
+    // if (!inputFile) {
+    //     std::cerr << "Error opening input file." << std::endl;
+    //     return 1;
+    // }
+    // std::string line;
+    // getline(inputFile, line);
+    // outputFile << line << std::endl;
+    // while (getline(inputFile, line)) {
+    //     std::stringstream ss(line);
+    //     std::string element;
+    //     std::vector<std::string> elements;
+    //     while (getline(ss, element, '\t')) {
+    //         elements.push_back(element);
+    //     }
+    //     if (std::stod(elements[2]) >= 100000) {
+    //         acceptableMovies.push_back(elements[0]);
+    //         outputFile << line << std::endl;
+    //     }
+    // }
+    // inputFile.close();
+    // outputFile.close();
+    // 1.2: trim movies by ratings
+    // std::ifstream inputFile2("../helper_src/data/title.basics.tsv");
+    // std::ofstream outputFile2("../helper_src/data/title.basics_2.tsv");
+    // if (!inputFile2) {
+    //     std::cerr << "Error opening input file." << std::endl;
+    //     return 1;
+    // }
+    // getline(inputFile2, line);
+    // outputFile2 << line << std::endl;
+    // while (getline(inputFile2, line)) {
+    //     std::stringstream ss(line);
+    //     std::string element;
+    //     std::vector<std::string> elements;
+    //     while (getline(ss, element, '\t')) {
+    //         elements.push_back(element);
+    //     }
+    //     if (std::find(acceptableMovies.begin(), acceptableMovies.end(), 
+    //         elements[0]) != acceptableMovies.end()) {
+    //         outputFile2 << line << std::endl;
+    //     }
+    // }
+    // inputFile2.close();
+    // outputFile2.close();
+    // std::ifstream inputFile3("../helper_src/data/title.crew.tsv");
+    // std::ofstream outputFile3("../helper_src/data/title.crew_2.tsv");
+    // if (!inputFile3) {
+    //     std::cerr << "Error opening input file." << std::endl;
+    //     return 1;
+    // }
+    // getline(inputFile3, line);
+    // outputFile3 << line << std::endl;
+    // while (getline(inputFile3, line)) {
+    //     std::stringstream ss(line);
+    //     std::string element;
+    //     std::vector<std::string> elements;
+    //     while (getline(ss, element, '\t')) {
+    //         elements.push_back(element);
+    //     }
+    //     if (std::find(acceptableMovies.begin(), acceptableMovies.end(), 
+    //         elements[0]) != acceptableMovies.end()) {
+    //         outputFile3 << line << std::endl;
+    //     }
+    // }
+    // inputFile3.close();
+    // outputFile3.close();
+    // std::ifstream inputFile4("../helper_src/data/title.principals.tsv");
+    // std::ofstream outputFile4("../helper_src/data/title.principals_2.tsv");
+    // if (!inputFile4) {
+    //     std::cerr << "Error opening input file." << std::endl;
+    //     return 1;
+    // }
+    // getline(inputFile4, line);
+    // outputFile4 << line << std::endl;
+    // while (getline(inputFile4, line)) {
+    //     std::stringstream ss(line);
+    //     std::string element;
+    //     std::vector<std::string> elements;
+    //     while (getline(ss, element, '\t')) {
+    //         elements.push_back(element);
+    //     }
+    //     if (std::find(acceptableMovies.begin(), acceptableMovies.end(), 
+    //         elements[0]) != acceptableMovies.end()) {
+    //         outputFile4 << line << std::endl;
+    //     }
+    // }
+    // inputFile4.close();
+    // outputFile4.close();
+    // std::ifstream inputFile45("../helper_src/data/title.principals_2.tsv");
+    // std::ofstream outputFile45("../helper_src/data/title.principals_3.tsv");
+    // if (!inputFile45) {
+    //     std::cerr << "Error opening input file." << std::endl;
+    //     return 1;
+    // }
+    // getline(inputFile45, line);
+    // while (getline(inputFile45, line)) {
+    //     std::stringstream ss(line);
+    //     std::string element;
+    //     std::vector<std::string> elements;
+    //     while (getline(ss, element, '\t')) {
+    //         elements.push_back(element);
+    //     }
+    //     if (elements[3] == "director" || elements[3] == "composer" 
+    //         || elements[3] == "actor" || elements[3] == "actress") {
+    //         outputFile45 << line << std::endl;
+    //     }
+    // }
+    // inputFile45.close();
+    // outputFile45.close();
+    // std::set<std::string> acceptableNames;
+    // std::ifstream inputFile5("../helper_src/data/title.principals_3.tsv");
+    // if (!inputFile5) {
+    //     std::cerr << "Error opening input file." << std::endl;
+    //     return 1;
+    // }
+    // getline(inputFile5, line);
+    // while (getline(inputFile5, line)) {
+    //     std::stringstream ss(line);
+    //     std::string element;
+    //     std::vector<std::string> elements;
+    //     while (getline(ss, element, '\t')) {
+    //         elements.push_back(element);
+    //     }
+    //     if (std::find(acceptableMovies.begin(), acceptableMovies.end(), 
+    //         elements[0]) != acceptableMovies.end()) {
+    //         acceptableNames.insert(elements[2]);
+    //     }
+    // }
+    // inputFile5.close();
+    // std::ifstream inputFile6("../helper_src/data/name.basics.tsv");
+    // std::ofstream outputFile6("../helper_src/data/name.basics_2.tsv");
+    // if (!inputFile6) {
+    //     std::cerr << "Error opening input file." << std::endl;
+    //     return 1;
+    // }
+    // getline(inputFile6, line);
+    // outputFile6 << line << std::endl;
+    // while (getline(inputFile6, line)) {
+    //     std::stringstream ss(line);
+    //     std::string element;
+    //     std::vector<std::string> elements;
+    //     while (getline(ss, element, '\t')) {
+    //         elements.push_back(element);
+    //     }
+    //     if (std::find(acceptableNames.begin(), acceptableNames.end(), 
+    //         elements[0]) != acceptableNames.end()) {
+    //         outputFile6 << line << std::endl;
+    //     }
+    // }
+    // inputFile6.close();
+    // outputFile6.close();
+    // Step 2: initiating DB
+    HelperDB helper_db;
+    helper_db.createTables();
+    helper_db.add_file_data("../helper_src/data/title.ratings_2.tsv", "ratings");
+    helper_db.add_file_data("../helper_src/data/name.basics_2.tsv", "name");
+    helper_db.add_file_data("../helper_src/data/title.basics_2.tsv", "media");
+    helper_db.add_file_data("../helper_src/data/title.principals_3.tsv", "profs");
+    helper_db.add_file_data("../helper_src/data/title.crew_2.tsv", "directors");
+}
--- helper_src/code/helper.h
+++ helper_src/code/helper.h
@@ -1,16 +1,20 @@
+#include <format>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <sqlite3.h>
-#include <format>
+#include <map>
 
-class CinemaDB
+class HelperDB
 {
 public:
-    CinemaDB(const std::string& db_path = "../data/im.db")
+    HelperDB(const std::string& db_path = "../data/im.db")
     {
         exit = sqlite3_open(db_path.c_str(), &db);
     }
-    ~CinemaDB() 
+    ~HelperDB() 
     {
         sqlite3_close(db);
     }
@@ -18,7 +22,7 @@ public:
     void createTables() 
     {
         char* creationError;
-        std::string query_name_basics = "CREATE TABLE IF NOT EXISTS people("
+        std::string query_name_basics = "CREATE TABLE IF NOT EXISTS name("
                                         "nconst TEXT PRIMARY KEY,"
                                         "primaryName TEXT,"
                                         "birthYear INT,"
@@ -31,85 +35,90 @@ public:
                                         "primaryTitle TEXT,"
                                         "isAdult BOOL,"
                                         "startYear INT,"
+                                        "endYear INT,"
                                         "runtimeMinutes INT,"
                                         "genres TEXT);";
-        std::string query_jobs = "CREATE TABLE IF NOT EXISTS jobs("
-                                        "ID INT PRIMARY KEY AUTOINCREMENT,"
-                                        "tconst TEXT,"
-                                        "nconst TEXT,"
-                                        "CATEGORY TEXT,"
-                                        "JOB TEXT,"
-                                        "CHARACTERS TEXT);";
+        std::string query_profs = "CREATE TABLE IF NOT EXISTS profs("
+                                "ID INT PRIMARY KEY,"
+                                "tconst TEXT,"
+                                "nconst TEXT,"
+                                "category TEXT,"
+                                "prof TEXT,"
+                                "characters TEXT);";
         std::string query_directors = "CREATE TABLE IF NOT EXISTS directors("
                                     "tconst TEXT PRIMARY KEY,"
                                     "directors TEXT);";
         std::string query_ratings = "CREATE TABLE IF NOT EXISTS ratings("
                                     "tconst TEXT PRIMARY KEY,"
-                                    "averageRatings FLOAT,"
+                                    "averageRating REAL,"
                                     "numVotes INT);";
-        exit = sqlite3_exec(db, query_name_basics.c_str(), NULL, NULL, &creationError);
-        exit = sqlite3_exec(db, query_film_basics.c_str(), NULL, NULL, &creationError);
-        exit = sqlite3_exec(db, query_jobs.c_str(), NULL, NULL, &creationError);
-        exit = sqlite3_exec(db, query_directors.c_str(), NULL, NULL, &creationError);
-        exit = sqlite3_exec(db, query_ratings.c_str(), NULL, NULL, &creationError);
-    }
-
-    void add_name_basics(std::string nconst, std::string primaryName, int birthYear,
-                            int deathYear, std::string primaryProfession, 
-                            std::string knownForTitles)
-    {
-        char* injectionError;
-        std::string query = std::format("INSERT INTO people (nconst, primaryName, birthYear, "
-                                        "deathYear, primaryProfession, knownForTitles) VALUES"
-                                        " ('{}', '{}', {}, {}, '{}', '{}')",
-                                        nconst, primaryName, birthYear, deathYear, 
-                                        primaryProfession, knownForTitles);
-        exit = sqlite3_exec(db, query.c_str(), NULL, NULL, &injectionError);
-    }
-    void add_film_basics(std::string tconst, std::string titleType, std::string primaryTitle, 
-                            bool isAdult, int startYear, int runtimeMinutes, std::string genres)
-    {
-        char* injectionError;
-        std::string query = std::format("INSERT INTO media (tconst, titleType, primaryTitle, "
-                                        "isAdult, startYear, runtimeMinutes, genres) VALUES"
-                                        " ('{}', '{}', '{}', {}, {}, {}, '{}')",
-                                        tconst, titleType, primaryTitle, isAdult, startYear, 
-                                        runtimeMinutes, genres);
-        exit = sqlite3_exec(db, query.c_str(), NULL, NULL, &injectionError);
-    }
-    void add_jobs(std::string tconst, std::string nconst, std::string category, 
-                    std::string job, std::string characters)
-    {
-        char* injectionError;
-        std::string query = std::format("INSERT INTO jobs (tconst, nconst, category, job, "
-                                        "characters) VALUES"
-                                        " ('{}', '{}', '{}', '{}', '{}')",
-                                        tconst, nconst, category, job, characters);
-        exit = sqlite3_exec(db, query.c_str(), NULL, NULL, &injectionError);
-    }
-    void add_directors(std::string tconst, std::string directors)
-    {
-        char* injectionError;
-        std::string query = std::format("INSERT INTO directors (tconst, directors) VALUES"
-                                        " ('{}', '{}')",
-                                        tconst, directors);
-        exit = sqlite3_exec(db, query.c_str(), NULL, NULL, &injectionError);
-    }
-    void add_ratings(std::string tconst, float averageRating, int numVotes)
-    {
-        char* injectionError;
-        std::string query = std::format("INSERT INTO ratings (tconst, averageRating, numvotes) VALUES"
-                                        " ('{}', {}, {})",
-                                        tconst, averageRating, numVotes);
-        exit = sqlite3_exec(db, query.c_str(), NULL, NULL, &injectionError);
+        exit = sqlite3_exec(this->db, query_name_basics.c_str(), NULL, NULL, &creationError);
+        exit = sqlite3_exec(this->db, query_film_basics.c_str(), NULL, NULL, &creationError);
+        exit = sqlite3_exec(this->db, query_profs.c_str(), NULL, NULL, &creationError);
+        exit = sqlite3_exec(this->db, query_directors.c_str(), NULL, NULL, &creationError);
+        exit = sqlite3_exec(this->db, query_ratings.c_str(), NULL, NULL, &creationError);
     }
 
     void sql_exec(std::string query, sqlite3_callback callback, void* pArg)
     {
         char* queryError;
-        exit = sqlite3_exec(db, query.c_str(), callback, pArg, &queryError);
+        exit = sqlite3_exec(this->db, query.c_str(), callback, pArg, &queryError);
+    }
+
+    void add_file_data(std::string filepath, std::string table)
+    // the datamap functions as following:
+    // -1 - this column is ignored during parsing
+    // 0 - this column holds a string value
+    // 1 - this column holds a numeric value
+    // 2 - this column holds a boolean value
+    // 3 - this column holds a double value
+    {
+        std::vector<int> datamap = dataMap.at(table);
+        char* fileError;
+        std::ifstream file(filepath);
+        if (!file.is_open()) {
+            exit = sqlite3_exec(NULL, NULL, NULL, NULL, &fileError);
+        }
+        std::string line;
+        std::getline(file, line);
+        while (getline(file, line))
+        {
+            std::istringstream iss(line);
+            std::string value;
+            std::ostringstream oss_sql;
+            int counter = 0;
+            while (getline(iss, value, '\t')) {
+                if (counter != 0 and datamap[counter] != -1) oss_sql << ", ";
+                if (value == "\\N" and datamap[counter] != -1) oss_sql << "NULL";
+                else if (datamap[counter] == 0) oss_sql << "'" << value << "'";
+                else if (datamap[counter] == 1) oss_sql << value;
+                else if (datamap[counter] == 2) 
+                {
+                    oss_sql << (value == "1" ? "TRUE" : "FALSE");
+                } 
+                counter++;
+            }
+            std::string insert = oss_sql.str();
+            std::string to_format = this->queryMap.at(table);
+            std::string formatted = to_format + insert + ");";
+            this->sql_exec(formatted, NULL, NULL);
+        }
     }
 private:
     sqlite3* db;
     int exit;
+    const std::unordered_map<std::string, std::string> queryMap = {
+        {"name", "INSERT INTO name (nconst, primaryName, birthYear, deathYear, primaryProfession, knownForTitles) VALUES ("},
+        {"media", "INSERT INTO media (tconst, titleType, primaryTitle, isAdult, startYear, endYear, runtimeMinutes, genres) VALUES ("},
+        {"profs", "INSERT INTO profs (tconst, nconst, category, prof, characters) VALUES ("},
+        {"directors", "INSERT INTO directors (tconst, directors) VALUES ("},
+        {"ratings", "INSERT INTO ratings (tconst, averageRating, numvotes) VALUES ("}
+    };
+    const std::unordered_map<std::string, std::vector<int>> dataMap = {
+        {"name", {0, 0, 1, 1, 0, 0}},
+        {"media", {0, 0, 0, -1, 2, 1, 1, 1, 0}},
+        {"profs", {0, -1, 0, 0, 0, 0}},
+        {"directors", {0, 0, -1}},
+        {"ratings", {0, 1, 1}}
+    };
 };
