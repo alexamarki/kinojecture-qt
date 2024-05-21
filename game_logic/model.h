e64e1305996e14f7568d873e3f6e726556d60460
alexamarki <amarkin2006@gmail.com>
2024-05-21 23:20:59 +0300

    model.h continuation

--- game_logic/model.h
+++ game_logic/model.h
@@ -4,26 +4,60 @@
 #include <QObject>
 #include <QJsonObject>
 #include <QJsonArray>
+#include <QRandomGenerator>
+#include <QTime>
 
-class Model : public QObject {
+class Model : public QObject 
+{
     Q_OBJECT
 public:
     Model(QObject *parent = nullptr);
 
     void initGame();
     void loadData(const std::vector<std::pair<std::string, std::string>>& data);
-    bool checkGuess(std::string card_id, bool isPrimaryPlayer);
+    bool checkGuess(int cardNum, bool isPrimaryPlayer);
     void updateTurnNum(bool isPrimaryPlayer);
-    int getPlayerCardId(bool isPrimaryPlayer) const;
+    void updateScores();
+    int getPlayerCardId(bool isPrimaryPlayer);
 
 signals:
     void updateEvent();
 
 private:
-    QJsonArray dataList;
+    std::vector<std::pair<std::string, std::string>> dataList;
+    std::pair<int, int> ids;
     std::pair<int, int> turns;
     std::pair<int, int> scores;
 
 };
+
+void Model::initGame() 
+{
+    QRandomGenerator generator = QRandomGenerator(static_cast<uint>(QTime::currentTime().msec()));
+    ids.first = generator.generate() % dataList.size();
+    ids.second = generator.generate() % dataList.size();
+    emit updateEvent();
+}
+
+bool Model::checkGuess(int cardNum, bool isPrimaryPlayer) 
+{
+    int checkNum = isPrimaryPlayer ? ids.first : ids.second;
+    bool correctGuess = (cardNum == checkNum);
+    updateTurnNum(isPrimaryPlayer);
+    return correctGuess;
+} 
+
+void Model::updateTurnNum(bool isPrimaryPlayer) 
+{
+    if (isPrimaryPlayer)
+        turns.first++;
+    else
+        turns.second++;
+}
+
+int Model::getPlayerCardId(bool isPrimaryPlayer)
+{
+    return isPrimaryPlayer ? ids.first: ids.second;
+}
 
 #endif
